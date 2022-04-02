58fc43b417a87fbc7363bb056b328795c7ef0984
Dmitry Yaniuk <ezzyfizzy27@gmail.com>
2022-04-02 23:42:27 +0300

    Refactor UnitTests library name to be consistent with main library

---
+++ IdentityWebApi.UnitTests/Shared/Controllers/UserControllerTestsDataFactory.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace IdentityWebApi.UnitTests.Shared.Controllers;
+
+public static class UserControllerTestsDataFactory
+{
+    public static ClaimsPrincipal GetUserIdentity(
+        string userId = default,
+        string email = default,
+        string[] roles = default
+    )
+    {
+        var userClaims = new List<Claim>();
+
+        if (!string.IsNullOrWhiteSpace(userId))
+        {
+            userClaims.Add(new Claim(ClaimTypes.NameIdentifier, userId));
+        }
+
+        if (!string.IsNullOrWhiteSpace(email))
+        {
+            userClaims.Add(new Claim(ClaimTypes.Email, email));
+        }
+
+        if (roles != null && roles.Any())
+        {
+            userClaims.Add(new Claim(ClaimTypes.Role, string.Join(",", roles)));
+        }
+
+        return new ClaimsPrincipal(new ClaimsIdentity(userClaims));
+    }
+}
