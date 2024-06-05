5d6745906f52ade9951d76b4812c79f33bcae505
Dmitry Yaniuk <ezzyfizzy27@gmail.com>
2024-06-05 23:57:46 +0300

    Add overloads to "IsNullOrEmpty" for collections with more specific types for performance optimisation

--- src/IdentityWebApi/Core/Utilities/Extensions.cs
+++ src/IdentityWebApi/Core/Utilities/Extensions.cs
@@ -8,13 +8,29 @@ namespace IdentityWebApi.Core.Utilities;
 public static class Extensions
 {
     /// <summary>
-    /// Checks whether collection is null or empty.
+    /// Checks whether <see cref="IEnumerable{T}" /> collection is null or empty.
     /// </summary>
     /// <param name="collection">Instance of <see cref="IEnumerable{T}"/>.</param>
     /// <typeparam name="T">Collection type.</typeparam>
-    /// <returns>
-    /// Boolean value indicating whether collection is null or empty.
-    /// </returns>
+    /// <returns>Boolean value indicating whether <see cref="IEnumerable{T}" /> collection is null or empty.</returns>
     public static bool IsNullOrEmpty<T>(this IEnumerable<T> collection) =>
         collection == null || !collection.GetEnumerator().MoveNext();
+
+    /// <summary>
+    /// Checks whether <see cref="ICollection{T}" /> collection is null or empty.
+    /// </summary>
+    /// <param name="collection">Instance of <see cref="ICollection{T}"/>.</param>
+    /// <typeparam name="T">Collection type.</typeparam>
+    /// <returns>Boolean value indicating whether <see cref="ICollection{T}" /> is null or empty.</returns>
+    public static bool IsNullOrEmpty<T>(this ICollection<T> collection) =>
+        collection == null || collection.Count == 0;
+
+    /// <summary>
+    /// Checks whether <see cref="ISet{T}" /> collection is null or empty.
+    /// </summary>
+    /// <param name="collection">Instance of <see cref="ISet{T}"/>.</param>
+    /// <typeparam name="T">Collection type.</typeparam>
+    /// <returns>Boolean value indicating whether <see cref="ISet{T}" /> is null or empty.</returns>
+    public static bool IsNullOrEmpty<T>(this ISet<T> collection) =>
+        collection == null || collection.Count == 0;
 }
